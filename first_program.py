bbca697b3f15d38d0a117ffb1217a3ed83c51b88
Adaliss <ada96liss@gmail.com>
2024-10-13 22:46:25 +0200

    Update first_program.py

--- first_program.py
+++ first_program.py
@@ -8,35 +8,22 @@
 # else:
 #   print('Error')
 import math
-
-
 def calculate_factorial(n):
-    """Вычисляет факториал заданного положительного целого числа."""
     return math.factorial(n)
-
-
 def main():
     while True:
         user_input = input("Введите положительное целое число: ")
-
         try:
-            # Пробуем преобразовать ввод в целое число
             number = int(user_input)
-
             if number < 0:
                 print("Ошибка: Введите положительное целое число.")
                 continue
-
-            # Вычисляем факториал и выводим результат
             factorial_result = calculate_factorial(number)
             print(f"Факториал числа {number} равен {factorial_result}.")
             break
-
         except ValueError:
             print(
                 "Ошибка: Ввод должен быть целым числом. Пожалуйста, попробуйте снова."
             )
-
-
 if __name__ == "__main__":
     main()
